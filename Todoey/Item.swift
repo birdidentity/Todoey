fd30bb541f1c49372872ff449e25796637f92f19
Vladyslav Boiko <bird.identity@gmail.com>
2020-05-15 15:11:05 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Vladislav Boyko on 15.05.2020.
+//  Copyright © 2020 Vladislav Boyko. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
